ff05065275f50d155f255da344d3d7532d1ba15f
Itzhak Levy <itzhaklevydev@gmail.com>
2025-08-13 17:56:24 +0300

    Add UseStateCounter component and remove unnecessary CRA files
    - Limit count to maximum of 10 and minimum of 0

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import UseStateCounter from "./components/UseStateCounter";
 
 function App() {
-  return <div className="App">React Hooks 1</div>;
+  return (
+    <div className="App">
+      <UseStateCounter />
+    </div>
+  );
 }
 
 export default App;
